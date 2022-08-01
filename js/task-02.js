ab77455f1d603fa1933192792fa27c469c14306c
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-01 17:45:59 +0300

    Task 02 is done

--- js/task-02.js
+++ js/task-02.js
@@ -2,7 +2,20 @@ const ingredients = [
   'Potatoes',
   'Mushrooms',
   'Garlic',
-  'Tomatos',
+  'Tomatoes',
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("#ingredients");
+console.log(listEl);
+
+const elements = ingredients.map(ingredient => {
+  const tagEl = document.createElement("li");
+  tagEl.classList.add("item");
+  tagEl.textContent = ingredient;
+
+  return tagEl;
+})
+// console.log(elements);
+listEl.append(...elements);
