f4cdbf86dcaf2aca29fd1415043268df1189992a
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-06 17:02:39 +0300

    Task 09 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBth = document.querySelector('.change-color');
+const background = document.querySelector('.color');
+
+changeColorBth.addEventListener('click', onChangeColorBthClick);
+
+function onChangeColorBthClick() {
+  const bgColorName = getRandomHexColor();
+  document.body.style.backgroundColor = bgColorName;
+  background.textContent = bgColorName;
+  console.log(background.textContent);
+}
+
+
+
+
+
