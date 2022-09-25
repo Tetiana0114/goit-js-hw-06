953e9807fbd1124d3a0065adc2a6c7be7a6e5472
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-09-26 00:29:26 +0300

    Task 10 done(needs some fix)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector('input'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+}
+
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.createBtn.addEventListener('click', createBoxes);
+
+function destroyBoxes() {
+  refs.boxesEl.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  amount = Number(refs.inputEl.value);
+  for (let i = 0; i < amount; i++) {
+    const size = `${i * 10 + 30}px`;
+    const markup = document.createElement('div');
+    markup.style.width = size;
+    markup.style.height = size;
+    markup.style.marginTop = '20px'
+    markup.style.backgroundColor = getRandomHexColor();
+    refs.boxesEl.append(markup);
+  }
+}
